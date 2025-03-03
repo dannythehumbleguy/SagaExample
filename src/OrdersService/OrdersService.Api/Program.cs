c521f30bacd7ec405b8322ed324aa42efd543e74
Danil Zhiltsov <dannygam47@gmail.com>
2025-03-04 01:50:55 +0300

    [#1] fix docker-compose.yml, add swagger

--- src/OrdersService/OrdersService.Api/Program.cs
+++ src/OrdersService/OrdersService.Api/Program.cs
@@ -1,23 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Orders Service API", Version = "v1" });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.MapOpenApi();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Orders Service API V1");
+    c.RoutePrefix = "swagger";
+});
 
+
+app.UseRouting();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
